b933ae57339ef32ae643477a7181627da8bf4a16
adilamin-dev <adilamin537@gmail.com>
2023-11-02 20:49:27 +0600

    menu

--- src/modules/Menu.jsx
+++ src/modules/Menu.jsx
@@ -4,10 +4,10 @@ const Menu = () => {
     return (
         <div>
             <ul>
-                <li><NavLink className={({isActive})=>isActive? 'navActive' : 'NavDeactive'} to="/">Home</NavLink></li>
-                <li><NavLink className={({isActive})=>isActive? 'navActive' : 'NavDeactive'} to="/post">Post</NavLink></li>
-                <li><NavLink className={({isActive})=>isActive? 'navActive' : 'NavDeactive'} to="/Products">Products</NavLink></li>
-                <li><NavLink className={({isActive})=>isActive? 'navActive' : 'NavDeactive'} to="/profile/Adil Amin">Profile</NavLink></li>
+                <li><NavLink className={({isActive})=>isActive? 'navActive' : 'NavDeactive'} to="/study-react/">Home</NavLink></li>
+                <li><NavLink className={({isActive})=>isActive? 'navActive' : 'NavDeactive'} to="/study-react/post">Post</NavLink></li>
+                <li><NavLink className={({isActive})=>isActive? 'navActive' : 'NavDeactive'} to="/study-react/Products">Products</NavLink></li>
+                <li><NavLink className={({isActive})=>isActive? 'navActive' : 'NavDeactive'} to="/study-react/profile/Adil Amin">Profile</NavLink></li>
             </ul>
         </div>
     );
